7f5b1afdadb2a2eb50821cd67418e62d5ba93f6d
Sunny <sunnyrathaur2444@gmail.com>
2022-12-01 14:43:26 +0530

    Add Update And Delete Option

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -31,16 +31,41 @@ class StudentController {
 
     }
 
-    static editDoc = (req, res) => {
-        res.render("edit")
+    static editDoc = async (req, res) => {
+        // console.log(req.params.id)
+        try {
+            const result = await StudentModle.findById(req.params.id)
+            // console.log(result)
+            res.render("edit",{data:result})
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
 
-    static updateDocById = (req, res) => {
+    static updateDocById = async (req, res) => {
+        // console.log(req.params.id);
+        // console.log(req.body);
+        try {
+            const result = await StudentModle.findByIdAndUpdate(req.params.id, req.body)
+            // console.log(result)
+        } catch (error) {
+            console.log(error)
+            
+        }
         res.redirect("/student")
     }
 
-    static deleteDocById = (req, res) => {
-        res.redirect("/student")
+    static deleteDocById = async (req, res) => {
+        // console.log(req.params.id);
+        try {
+            const result = await StudentModle.findByIdAndDelete(req.params.id)
+            // console.log(result)
+            res.redirect("/student")
+        } catch (error) {
+            console.log(error)
+            
+        }
     }
 }
 
